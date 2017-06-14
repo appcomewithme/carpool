1d351a409e528d4c9e2e931053cdfd9346e77828
akaushik38 <akaushik3893@gmail.com>
2017-06-14 17:00:02 +0530

    first commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class Login {
+
+  backgrounds = [
+    "assets/img/background/background-1.jpg",
+    "assets/img/background/background-2.jpg",
+    "assets/img/background/background-3.jpg",
+    "assets/img/background/background-4.jpg"
+  ]
+  public loginForm: any;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.compose([Validators.minLength(6),
+      Validators.required])]
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello LoginBackgroundSlider Page');
+  }
+
+  openResetPassword() {
+  }
+
+  doLogin() {
+    if (!this.loginForm.valid) {
+      console.log("Invalid or empty data");
+    } else {
+      let userEmail = this.loginForm.value.email;
+      let userPassword = this.loginForm.value.password;
+
+      console.log('user data', userEmail, userPassword);
+    }
+  }
+  CreateAccount() {
+
+  }
+
+}
